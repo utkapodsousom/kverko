bd84866280e2827cc413916929f67510cdb76fa4
utkapodsousom <utkapodsousom@gmail.com>
2016-11-16 20:05:57 +0300

    added cards view and its router

--- app.js
+++ app.js
@@ -3,10 +3,40 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var kverkoRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
+
 app.set('view engine', 'jade');
+
+kverkoRouter.route('/')
+  .get(function(req, res){
+    res.render('cards', {
+        title: 'Cards',
+        nav: [{
+            Link: '/main',
+            Text: 'Главная'
+        }, {
+            Link: '/cards',
+            Text: 'Штамп-визитки'
+        }, {
+            Link: '/layout',
+            Text: 'Индивидуальный макет'
+        }, {
+            Link: '/gallery',
+            Text: 'Галерея'
+        }, {
+            Link: '/order',
+            Text: 'Заказать'
+        }, {
+            Link: '/about',
+            Text: 'О нас'
+        }]
+    });
+  });
+
+app.use('/cards', kverkoRouter)
 
 app.get('/', function(req, res) {
     res.render('index', {
